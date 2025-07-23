b4eec04692f2fc99285ec7fffa13d3d26b46c6ef
audirline <thanh.audi@outlook.fr>
2025-07-22 22:42:49 -0400

    Update home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -6,38 +6,41 @@ describe('Spring Music App', () => {
     cy.contains('Spring Music').should('exist');
   });
 
-it('adds an album', () => {
-  cy.visit('https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/')
-  cy.get('a[ng-click="addAlbum()"]').click()
-  cy.get('#title').type('Test Album')
-  cy.get('#artist').type('Test Artist')
-  cy.get('#releaseYear').type('2024')
-  cy.get('#genre').type('Test Genre')
-  cy.get('button[ng-click="ok()"]').click()
-  cy.contains('Test Album').should('exist')
-})
+  it('ajoute un album "Test Album"', () => {
+    cy.visit(baseUrl);
+    cy.get('a[ng-click="addAlbum()"]').click();
+    cy.get('#title').type('Test Album');
+    cy.get('#artist').type('Test Artist');
+    cy.get('#releaseYear').type('2024');
+    cy.get('#genre').type('Test Genre');
+    cy.get('button[ng-click="ok()"]').click();
+    cy.contains('Test Album').should('exist');
+  });
 
-it('adds an album', () => {
-  cy.visit('https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/')
-  cy.get('a[ng-click="addAlbum()"]').click()
-  cy.get('#title').type('Everything')
-  cy.get('#artist').type('Avril Lavigne')
-  cy.get('#releaseYear').type('2012')
-  cy.get('#genre').type('Pop')
-  cy.get('button[ng-click="ok()"]').click()
-  cy.contains('Everything').should('exist')
-})
+  it('ajoute un autre album "Everything"', () => {
+    cy.visit(baseUrl);
+    cy.get('a[ng-click="addAlbum()"]').click();
+    cy.get('#title').type('Everything');
+    cy.get('#artist').type('Avril Lavigne');
+    cy.get('#releaseYear').type('2012');
+    cy.get('#genre').type('Pop');
+    cy.get('button[ng-click="ok()"]').click();
+    cy.contains('Everything').should('exist');
+  });
 
+  it('supprime l’album "Test Album"', () => {
+    cy.visit(baseUrl);
 
-it('delete an album', () => {
-  cy.visit('https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/')
-  cy.contains('tr', 'Test Album').within(() => {
-    cy.get('.glyphicon-cog').click()
-    cy.contains('delete').click()
-  })
-  // Vérifie que l’album n’est plus dans la liste
-  cy.contains('Test Album').should('not.exist')
-})
+    // Vérifie que l'album existe avant suppression
+    cy.contains('tr', 'Test Album').should('exist');
 
+    cy.contains('tr', 'Test Album').within(() => {
+      cy.get('.glyphicon-cog').click();
+      cy.contains('delete').click();
+    });
+
+    // Vérifie que l’album a bien été supprimé
+    cy.contains('Test Album').should('not.exist');
+  });
 
 });
