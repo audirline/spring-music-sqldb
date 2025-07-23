ca399d9afcb9025686aa4715129cda76738a237c
audirline <thanh.audi@outlook.fr>
2025-07-23 10:26:58 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: true, // active l’enregistrement vidéo de chaque test
   e2e: {
     baseUrl: 'https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
     setupNodeEvents(on, config) {
-      // Tu peux ajouter des plugins Cypress ici si besoin
+      // plugins éventuels ici
     },
   },
 })
