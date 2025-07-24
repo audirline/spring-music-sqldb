68d8440c61f21a85f7fb721cbab60511b84a9d83
audirline <thanh.audi@outlook.fr>
2025-07-24 15:49:48 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
+    //supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       //require('cypress-mochawesome-reporter/plugin')(on);
     },
