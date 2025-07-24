dfdfd07ffe6cf43af1a9225e5418a7bee37b5893
audirline <thanh.audi@outlook.fr>
2025-07-23 22:25:17 -0400

    Update home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,12 @@
 describe('Spring Music App', () => {
   const baseUrl = 'https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/';
 
+  afterEach(function () {
+    // Capture automatique à la fin de chaque test, même en cas de succès
+    const testName = this.currentTest.title.replace(/\s+/g, '-').toLowerCase();
+    cy.screenshot(testName);
+  });
+
   it('affiche la page d’accueil', () => {
     cy.visit(baseUrl);
     cy.contains('Spring Music').should('exist');
@@ -17,25 +23,21 @@ describe('Spring Music App', () => {
     cy.contains('Test Album').should('exist');
   });
 
-it('modifie le nom d’un album "Test Album"', () => {
-  const oldName = 'Test Album';
-  const newName = 'Test Album Modified';
-  cy.visit('/');
-  // Trouver un élément visible contenant "Test Album"
-  cy.contains('span.ng-binding', oldName)
-    .filter(':visible') // Sélectionne uniquement les éléments visibles
-    .first() // Prend le premier
-    .click({ force: true }) // 1er clic
-    .click({ force: true }); // 2e clic pour activer le champ
-  // Champ input visible pour édition
-  cy.get('input[name="title"]:visible')
-    .clear()
-    .type(`${newName}{enter}`);
-  // Vérifie que le nouveau nom apparaît
-  cy.contains('span.ng-binding', newName).should('exist');
-});
+  it('modifie le nom d’un album "Test Album"', () => {
+    const oldName = 'Test Album';
+    const newName = 'Test Album Modified';
+    cy.visit(baseUrl);
+    cy.contains('span.ng-binding', oldName)
+      .filter(':visible')
+      .first()
+      .click({ force: true })
+      .click({ force: true });
+    cy.get('input[name="title"]:visible')
+      .clear()
+      .type(`${newName}{enter}`);
+    cy.contains('span.ng-binding', newName).should('exist');
+  });
 
-  
   it('ajoute un autre album "Everything"', () => {
     cy.visit(baseUrl);
     cy.get('a[ng-click="addAlbum()"]').click();
@@ -47,63 +49,46 @@ it('modifie le nom d’un album "Test Album"', () => {
     cy.contains('Everything').should('exist');
   });
 
+  it('modifie le nom d’un album Everything', () => {
+    const oldName2 = 'Everything';
+    const newName2 = 'Everything Modified';
+    cy.visit(baseUrl);
+    cy.contains('span.ng-binding', oldName2)
+      .filter(':visible')
+      .first()
+      .click({ force: true })
+      .click({ force: true });
+    cy.get('input[name="title"]:visible')
+      .clear()
+      .type(`${newName2}{enter}`);
+    cy.contains('span.ng-binding', newName2).should('exist');
+  });
 
-it('modifie le nom d’un album Everything', () => {
-  const oldName2 = 'Everything';
-  const newName2 = 'Everything Modified';
-  cy.visit('/');
-  // Trouver un élément visible contenant "Everything"
-  cy.contains('span.ng-binding', oldName2)
-    .filter(':visible') // Sélectionne uniquement les éléments visibles
-    .first() // Prend le premier
-    .click({ force: true }) // 1er clic
-    .click({ force: true }); // 2e clic pour activer le champ
-  // Champ input visible pour édition
-  cy.get('input[name="title"]:visible')
-    .clear()
-    .type(`${newName2}{enter}`);
-  // Vérifie que le nouveau nom apparaît
-  cy.contains('span.ng-binding', newName2).should('exist');
-});
+  it('supprime un album nommé "Test Album Modified"', () => {
+    const albumName = 'Test Album Modified';
+    cy.visit(baseUrl);
+    cy.contains('span.ng-binding', albumName)
+      .should('be.visible')
+      .parents('div.row.multi-columns-row > div > div')
+      .first()
+      .within(() => {
+        cy.get('.glyphicon-cog').click();
+        cy.contains('a', 'delete').click();
+      });
+    cy.contains(albumName).should('not.exist');
+  });
 
-
- it('supprime un album nommé "Test Album Modified"', () => {
-  const albumName = 'Test Album';
-  cy.visit(baseUrl);
-  // Trouver le span qui contient le nom de l'album
-  cy.contains('span.ng-binding', albumName)
-    .should('be.visible')
-    // remonte jusqu'à un parent qui contient tout l'album, par exemple le 4e ou 5e parent div (à ajuster)
-    .parents('div.row.multi-columns-row > div > div') 
-    .first() 
-    .within(() => {
-      // Clique sur la roue dentée pour options
-      cy.get('.glyphicon-cog').click();
-      // Clique sur "delete"
-      cy.contains('a', 'delete').click();
-    });
-  // Vérifie que l’album n’est plus visible1
-  cy.contains(albumName).should('not.exist');
-});
-
- it('supprime un album nommé "Everything Modified"', () => {
-  const albumName2 = 'Everything Modified';
-  cy.visit(baseUrl);
-  // Trouver le span qui contient le nom de l'album
-  cy.contains('span.ng-binding', albumName2)
-    .should('be.visible')
-    // remonte jusqu'à un parent qui contient tout l'album, par exemple le 4e ou 5e parent div (à ajuster)
-    .parents('div.row.multi-columns-row > div > div') 
-    .first() 
-    .within(() => {
-      // Clique sur la roue dentée pour options
-      cy.get('.glyphicon-cog').click();
-      // Clique sur "delete"
-      cy.contains('a', 'delete').click();
-    });
-  // Vérifie que l’album n’est plus visible1
-  cy.contains(albumName2).should('not.exist');
-});
-
-  
+  it('supprime un album nommé "Everything Modified"', () => {
+    const albumName2 = 'Everything Modified';
+    cy.visit(baseUrl);
+    cy.contains('span.ng-binding', albumName2)
+      .should('be.visible')
+      .parents('div.row.multi-columns-row > div > div')
+      .first()
+      .within(() => {
+        cy.get('.glyphicon-cog').click();
+        cy.contains('a', 'delete').click();
+      });
+    cy.contains(albumName2).should('not.exist');
+  });
 });
