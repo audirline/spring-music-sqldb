7e31932f23bdabb0d0c1de60cda3b9b7aa057741
audirline <thanh.audi@outlook.fr>
2025-07-24 15:42:29 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,19 +3,17 @@ const mochawesome = require('cypress-mochawesome-reporter/plugin')
 
 module.exports = defineConfig({
   video: true,
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'mocha-junit-reporter',
   reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: false,
-    html: true,
-    json: true
+    mochaFile: 'cypress/reports/junit/test-results.xml',
+    toConsole: true
   },
   e2e: {
     baseUrl: 'https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      //require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 })
