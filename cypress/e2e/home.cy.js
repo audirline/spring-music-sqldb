df7c34498de7b6892103cd04e0a5a4bd7367fbdb
audirline <thanh.audi@outlook.fr>
2025-07-25 14:48:30 -0400

    Update home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,17 +2,17 @@ describe('Spring Music App', () => {
   const baseUrl = 'https://spring-music-hgckhuf3gza0bvb2.canadacentral-01.azurewebsites.net/';
 
   afterEach(function () {
-    // Capture automatique à la fin de chaque test, même en cas de succès
+    // Screenshots for every test even if it passes
     const testName = this.currentTest.title.replace(/\s+/g, '-').toLowerCase();
     cy.screenshot(testName);
   });
 
-  it('affiche la page d’accueil', () => {
+  it('Display main page', () => {
     cy.visit(baseUrl);
     cy.contains('Spring Music').should('exist');
   });
 
-  it('ajoute un album "Test Album"', () => {
+  it('Add an album "Test Album"', () => {
     cy.visit(baseUrl);
     cy.get('a[ng-click="addAlbum()"]').click();
     cy.get('#title').type('Test Album');
@@ -23,7 +23,7 @@ describe('Spring Music App', () => {
     cy.contains('Test Album').should('exist');
   });
 
-  it('modifie le nom d’un album "Test Album"', () => {
+  it('Modify the name of the album "Test Album"', () => {
     const oldName = 'Test Album';
     const newName = 'Test Album Modified';
     cy.visit(baseUrl);
@@ -38,7 +38,7 @@ describe('Spring Music App', () => {
     cy.contains('span.ng-binding', newName).should('exist');
   });
 
-  it('ajoute un autre album "Everything"', () => {
+  it('Add another album "Everything"', () => {
     cy.visit(baseUrl);
     cy.get('a[ng-click="addAlbum()"]').click();
     cy.get('#title').type('Everything');
@@ -49,7 +49,7 @@ describe('Spring Music App', () => {
     cy.contains('Everything').should('exist');
   });
 
-  it('modifie le nom d’un album Everything', () => {
+  it('Modify the name of the album Everything', () => {
     const oldName2 = 'Everything';
     const newName2 = 'Everything Modified';
     cy.visit(baseUrl);
@@ -64,7 +64,7 @@ describe('Spring Music App', () => {
     cy.contains('span.ng-binding', newName2).should('exist');
   });
 
-  it('supprime un album nommé "Test Album Modified"', () => {
+  it('Remove the album "Test Album Modified"', () => {
     const albumName = 'Test Album Modified';
     cy.visit(baseUrl);
     cy.contains('span.ng-binding', albumName)
@@ -78,7 +78,7 @@ describe('Spring Music App', () => {
     cy.contains(albumName).should('not.exist');
   });
 
-  it('supprime un album nommé "Everything Modified"', () => {
+  it('Remove the album "Everything Modified"', () => {
     const albumName2 = 'Everything Modified';
     cy.visit(baseUrl);
     cy.contains('span.ng-binding', albumName2)
